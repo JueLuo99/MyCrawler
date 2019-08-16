7b73e67bbd3e50e7dcf8a71660e451759199ce4c
JueLuo <jueluo99@gmail.com>
2019-08-17 00:38:34 +0800

    完善代码逻辑

--- main.py
+++ main.py
@@ -7,9 +7,8 @@ import time
 downloadPath = os.path.abspath(os.path.dirname(__file__))
 downloadPath += os.path.sep + time.strftime("%Y%m%d_%H%M%S", time.localtime())
 
-# 创建工作空间
-if not(os.path.exists(downloadPath)):
-    os.mkdir(downloadPath)
+# 需要爬取的图片的 tag
+tag = ""
 
 def getPicURL(url):
     """
@@ -17,7 +16,7 @@ def getPicURL(url):
     """
     # 访问图片的详情页
     response = requests.get(url)
-    # 匹配出下载链接
+    # 匹配出原图的下载链接
     try:
         pattern = re.compile(r'<a id="image-resize-link" href="https://danbooru.donmai.us/data/[0-9a-z_\-]{0,}.(jpg|jpeg|png|bmp|gif)')
         picURL = str(pattern.search(response.text).group(0)).replace('<a id="image-resize-link" href="','')
@@ -27,27 +26,70 @@ def getPicURL(url):
         picURL = str(pattern.search(response.text).group(0)).replace('src="','')
     return picURL
 
+
+
 def downloadPic(url):
+    """
+    保存目标图片
+    """
     fileName = os.path.split(url)[1]
     with open(downloadPath+os.path.sep+fileName, 'wb') as file:
         file.write(requests.get(url).content)
 
-response = requests.get("https://danbooru.donmai.us/posts?utf8=✓&tags=sayori")
-# 匹配出当前页的每张图片的详情页并存入 idList
-pattern = re.compile(r'<article id="post_[0-9]{0,}')
-idList = pattern.findall(response.text)
 
-# 转换为该页面每张图片的详情页
-for i in range(len(idList)):
-    idList[i] = idList[i].replace('<article id="post_','https://danbooru.donmai.us/posts/')
 
-for i in idList:
-    print("正在爬取 " + i + " 页面的原图")
-    downloadURL = getPicURL(i)
+def getPostList(url):
+    """
+    获取目标页面的每个 post 的 URL 并返回一个 list
+    如果没有更多页面，返回的 list 的第一个元素将为 "NoMorePages!"
+    """
+    response = requests.get(url)
+    # 匹配出当前页的每张图片的详情页并存入 idList
+    pattern = re.compile(r'<article id="post_[0-9]{0,}')
+    idList = pattern.findall(response.text)
 
-    downloadPic(downloadURL)
-    print("下载完毕 " + downloadURL)
+    # 没有更多页面时将 list 的第一个元素设为 "NoMorePages!"
+    if len(idList)==0:
+        pattern = re.compile(r'<p>Nobody here but us chickens!</p>')
+        if pattern.search(response.text).group(0):
+            idList.append("NoMorePages!")
+            
 
-# print(idList)
+    # 转换为该页面每张图片的详情页
+    for i in range(len(idList)):
+        idList[i] = idList[i].replace('<article id="post_','https://danbooru.donmai.us/posts/')
+    return idList
+
+
+# 创建工作空间
+if not(os.path.exists(downloadPath)):
+    os.mkdir(downloadPath)
+print("创建目录 "+downloadPath)
+
+# 默认从第 1 页开始爬取
+page = 1
+
+while True:
+    # 构造目标网址，准备开始爬取
+    url = "https://danbooru.donmai.us/posts?tags=" + tag + "&page=" + str(page)
+    postList = getPostList(url)
+    if postList[0]=="NoMorePages!":
+        print("爬取完毕 已无更多页面，程序即将退出")
+        exit(0)
+    print("生成列表 " + url + "该页面共有 " + str(len(postList)) + " 张图片")
+    
+    # 开始爬取并下载图片
+    for post in postList:
+        print("正在爬取 " + post + " 页面的原图")
+        downloadURL = getPicURL(post)
+        print("开始下载 " + downloadURL)
+        downloadPic(downloadURL)
+        print("下载完毕 " + downloadURL)
+    
+    # 当前页面爬取完毕，开始准备爬取下一页
+    print("\n切换页面 当前页面爬取完毕，开始爬取第" + str(page) + "页\n")
+    page += 1
+
+
 
 
